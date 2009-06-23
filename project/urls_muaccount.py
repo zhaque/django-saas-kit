c4c5ce420dce9dc8e0a3686e26889426c96a208f
Maciej Pasternacki <maciej@pasternacki.net>
2009-06-23 17:46:21 +0200

    - Full registration support in muaccounts; display "sign up" link only in public muaccount sites, however.

--- project/urls_muaccount.py
+++ project/urls_muaccount.py
@@ -3,7 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', dict(template='account_index.html')),
     (r'^sorry/$', 'django.views.generic.simple.direct_to_template', dict(template='account_nam.html'), 'muaccounts_not_a_member'),
-    (r'^accounts/login/', 'django.contrib.auth.views.login'),
-    (r'^accounts/logout/', 'django.contrib.auth.views.logout', dict(next_page='/')),
+    (r'^accounts/', include('registration.urls')),
     (r'^admin/', include('muaccounts.urls')),
 )
